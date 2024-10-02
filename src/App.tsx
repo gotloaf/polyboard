22e3ba5437a576038cef82ca48e0b1bdb22e5d7a
gotloaf <gotloaf@users.noreply.github.com>
2024-10-02 22:42:19 +0000

    Input functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,32 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
+import github from './assets/github.svg'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [textContent, setTextContent] = useState("")
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="main-content-anchor">
+      <textarea
+        className="main-text"
+        value={textContent}
+        onChange={e => setTextContent(e.target.value)}
+      />
+      <div className="bottom-bar">
+
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+      <div className="selector">
+
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+      <a href="https://github.com/gotloaf/polyboard" target="_blank" className="credit">
+        <img src={github} className="logo small invert-when-dark" alt="GitHub Logo" />
+        <span>gotloaf/<span className="credit-repo-name">polyboard</span></span>
+      </a>
+      <div className="keyboard-display">
+
+      </div>
+    </div>
   )
 }
 
